534178c8c72f176c64dcea8e86b82ba04e8da22f
Nokeen <cordrepayne@gmail.com>
2022-11-05 15:43:24 -0500

    new cursor and disable text selection

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,13 @@
 $(window).on("load",function(){
     $(".loader-wrapper").fadeOut("slow");
 });
+const cursor = document.getElementById("cursor");
+document.onclick = e => {
+    cursor.classList.add('expand');
+    setTimeout(() =>{
+        cursor.classList.remove('expand');
+    }, 100)
+}
+    document.addEventListener('mousemove', e =>{
+        cursor.setAttribute("style", "top: "+(e.pageY-10)+"px; left: "+(e.pageX-10)+"px;")
+})
